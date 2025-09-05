40f25348b3a363c3b3347a0b30762a9670893000
Gino Grosso <ginogrosso21@gmail.com>
2025-09-05 11:50:02 -0300

    Configuración con Vite, React y .gitignore agregado

---
+++ Downloads/Actividad27-createelement-main/Actividad27-createelement-main/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Con lo visto hasta ahora</h1>
+      <h2>Replica este HTML</h2>
+      <h3>Usando</h3>
+      <h4>React.js</h4>
+      <footer>Suerte!</footer>
+    </>
+  );
+}
+
+export default App;
